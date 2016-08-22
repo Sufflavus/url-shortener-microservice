f1522c46d0789a10b21f7868f236a21306592936
Tania <tatashat@gmail.com>
2016-08-22 20:37:29 +0000

    + mongodb

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
+var mongo = require('mongodb').MongoClient;
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+mongo.connect('mongodb://localhost:27017/url-shortener', function (err, db) {
+
+    if (err) {
+        throw new Error('Database failed to connect!');
+    } else {
+        console.log('MongoDB successfully connected on port 27017.');
+    }
+
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+    
+    app.listen(8080, function () {
+        console.log('Example app listening on port 8080!');
+    });
+    
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
