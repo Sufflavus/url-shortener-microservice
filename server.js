e0cb3fab9a3301648e0eba1af9da2248aefcdbf2
Tania <tatashat@gmail.com>
2016-08-23 19:49:35 +0000

    + db url request

--- server.js
+++ server.js
@@ -10,7 +10,22 @@ mongo.connect('mongodb://localhost:27017/url-shortener', function (err, db) {
         console.log('MongoDB successfully connected on port 27017.');
     }
 
-    app.get('/', function (req, res) {
+    app.get('/:number', function (req, res) {
+        var urls = db.collection('urls');
+        var urlProjection = { '_id': false };
+        urls.findOne({ number: req.params.number }, urlProjection, function (err, doc) {
+            if (err) {
+                throw err;
+            }
+            
+            if(doc) {
+                res.redirect(doc.path);
+            } else {
+                res.json({
+                    "error": "This url is not in database."
+                });
+            }
+        });
         res.send('Hello World!');
     });
     
