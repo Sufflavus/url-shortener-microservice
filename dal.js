d4fbe22a9b5ac7d0b8a4eb6566f2fb7b02addbaf
Tania <tatashat@gmail.com>
2016-09-02 04:01:53 +0000

    * db access moved to dal

--- dal.js
+++ dal.js
@@ -2,34 +2,65 @@
 
 module.exports = Dal;
 
-function Dal () {
+function Dal (mongo) {
     var dbPath = "mongodb://localhost:27017/url-shortener";
     var urlsCollectionName = "urls";
     
-	this.getUrl = function (db, number) {
-        var urls = db.collection(urlsCollectionName);
-        var urlProjection = { '_id': false };
-        urls.findOne({ number: number }, urlProjection, function(err, doc) {
-            if(err) {
-                throw err;
-            }
-            
-            if(doc) {
-                return doc;
+	this.getUrl = function (number) {
+	    mongo.connect(dbPath, function (err, db) {
+            if (err) {
+                throw new Error('Database failed to connect!');
             } else {
-                return {
-                    "error": "This url is not in database."
-                };
+                console.log('MongoDB successfully connected on port 27017.');
             }
-        });
+	        
+	        var urls = db.collection(urlsCollectionName);
+        
+            urls.findOne({ number: number }, function (err, doc) {
+                //TODO: close connection
+                
+                if (err) {
+                    throw err;
+                }
+                
+                if(doc) {
+                    return doc;
+                } else {
+                    throw "This url is not in database.";
+                }
+            });
+	    });
 	};
 	
-	this.addUrl = function (db, url) {
-        var urls = db.collection(urlsCollectionName);
-        var cursor = urls.find({}, { number: 1 }).sort({ number: -1 }).limit(1);
-        var nextNumber = cursor.hasNext() ? cursor.next().number + 1 : 1;
-        var doc = { number: nextNumber, path: url };
-        urls.insertOne(doc);
-        return doc;
+	this.addUrl = function (url) {
+	    mongo.connect(dbPath, function (err, db) {
+            if (err) {
+                throw new Error('Database failed to connect!');
+            } else {
+                console.log('MongoDB successfully connected on port 27017.');
+            }
+	        
+	        var urls = db.collection(urlsCollectionName);
+        
+            urls.find({ $query: {}, $orderby: { number: -1 } }).limit(1).toArray(function(err, items) {
+                //TODO: close connection
+                
+                if (err) {
+                    throw err;
+                }
+                
+                var nextNumber = items.length ? items[0].number + 1 : 1;
+                
+                var doc = { number: nextNumber, path: url };
+                
+                urls.insertOne(doc, function(err, newDocs){
+                    if (err) {
+                        throw err;
+                    }
+                    
+                    return newDocs.ops[0];
+                });
+            });
+	    });
 	};
 }
