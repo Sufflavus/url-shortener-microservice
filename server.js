0762b69c20b7a46ee0aa0cd94baf011e4ee4df2d
Tania <tatashat@gmail.com>
2016-09-28 21:12:50 +0000

    * bug fix

--- server.js
+++ server.js
@@ -14,96 +14,21 @@ dal.connect(function() {
     });
 });
 
-/*mongo.connect('mongodb://localhost:27017/url-shortener', function(err, database) {
-    if (err) {
-        throw new Error('Database failed to connect!');
-    } else {
-        console.log('MongoDB successfully connected on port 27017.');
-    }
-
-    db = database;
-
-    // Start the application after the database connection is ready
-    app.listen(8080, function () {
-        console.log('Example app listening on port 8080!');
-    });
-});*/
-
 app.get('/favicon.ico', function (req, res) {
+});
+
+app.get('/new/*', function (req, res) {
+    var url = req.params[0];
+    var hostName = req.headers.host;
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    dal.addUrl(url, hostName, res);
 });
 
 app.get('/:number', function (req, res) {
     var number = +req.params.number;
     dal.getUrl(number, res);
-    
-    /*var urls = db.collection('urls');
-    
-    urls.findOne({ number: +req.params.number }, function (err, doc) {
-        if (err) {
-            throw err;
-        }
-        
-        if(doc) {
-            res.redirect(doc.path);
-        } else {
-            res.json({
-                "error": "This url is not in database."
-            });
-        }
-    });*/
-});
-
-app.get('/new/:url', function (req, res) {
-    console.log(req.params.url)
-    var url = req.params.url;
-    var hostName = req.headers.host;
-    dal.addUrl(url, hostName, res);
-    
-    /*var urls = db.collection('urls');
-    
-    urls.find({ $query: {}, $orderby: { number: -1 } }).limit(1).toArray(function(err, items) {
-        if (err) {
-            throw err;
-        }
-        
-        var nextNumber = items.length ? items[0].number + 1 : 1;
-        
-        var doc = { number: nextNumber, path: req.params.url };
-        
-        urls.insertOne(doc, function(err, newDocs){
-            if (err) {
-                throw err;
-            }
-            
-            var newItem = newDocs.ops[0];
-            var hostName = req.headers.host;
-            
-            res.json({ 
-                "original_url": newItem.path, 
-                "short_url": hostName + "/" + newItem.number
-            });    
-        });
-    });*/
 });
 
 app.get('/', function (req, res) {
   res.sendFile(path + '/public/index.html');
-});
-
-
-app.get('/*', function (req, res) {
-    //console.log(!req.params)
-    if(!req.params.length || req.params[0].indexOf("new/") !== 0) {
-        res.json({
-            "error": "This url is not in database."
-        });
-        return;
-    }
-    
-    
-    console.log(req.params)
-    var url = req.params[0].substring(4);
-    
-    var hostName = req.headers.host;
-    dal.addUrl(url, hostName, res);
 });
